4bc179d43a2fdd5b1e19cfc2969ee436562f0b1a
Benson Grotegut <gutblue@Bensons-Air.app.byu.edu>
2025-02-10 16:43:43 -0700

    my gf added her favorite movie

--- obj/Debug/net8.0/Mission6Assignment.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mission6Assignment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission6Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e76354910e83b9c8757e8604af5aaf95ffd92476")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission6Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission6Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
