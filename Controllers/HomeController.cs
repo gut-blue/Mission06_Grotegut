57c934c9b7acc671e90cac1cccb885e4a9aa5ac3
Benson Grotegut <gutblue@Bensons-MacBook-Air.local>
2025-02-21 17:32:47 -0700

    Updated project with new branch

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Mission6Assignment.Models;
 
 namespace Mission6Assignment.Controllers;
@@ -28,30 +29,99 @@ public class HomeController : Controller
     [HttpGet]
     public IActionResult AddMovieForm()
     {
+        ViewBag.Categories = _context.Categories.ToList();
+        
         var model = new Movie
         {
-            CategoryName = string.Empty,
-            MovieTitle = string.Empty, // Default empty string
-            MovieYear = null, // Default to current year
-            DirectorFirstName = string.Empty,
-            DirectorLastName = string.Empty,
-            MovieRating = "--Select a Rating--" // Default value for rating
+            CategoryId = 1,
+            Title = string.Empty, // Default empty string
+            Year = DateTime.Now.Year, // Default to current year
+            Director = string.Empty,
+            Rating = "--Select a Rating--",
+            Category = null,
+            Edited = false,
+            CopiedToPlex = false // Default value for rating
         };
         
+        // Passing new model, not working
         return View(model);
     }
 
     [HttpPost]
     public IActionResult AddMovieForm(Movie response)
     {
-        _context.Movies.Add(response);  // Adding "Movie" record to the database
-        _context.SaveChanges(); // Commit changes to the database
-        
-        if (!ModelState.IsValid)
+        if (ModelState.IsValid)
         {
+            _context.Movies.Add(response);  // Adding "Movie" record to the database
+            _context.SaveChanges(); // Commit changes to the database
+        
+            return View("Confirmation", response);
+        }
+        else // If Invalid Data
+        {
+            ViewBag.Categories = _context.Categories.ToList();
             return View(response);
         }
+    }
+
+    // First ever Linq query
+    public IActionResult Movielist()
+    {
+        var movies = _context.Movies
+            .Select(m => new 
+            {
+                m.MovieId,
+                m.Title,
+                m.Director,
+                m.Rating,
+                LentTo = m.LentTo ?? "N/A",  // Replace null with "N/A"
+                Notes = m.Notes ?? "N/A"     // Replace null with "N/A"
+            })
+            .ToList();
+
+        // Return the view with the movies data
+        return View("Movielist");  // Pass the movies data to the view
+    }
+
+    
+    // Edit Action Method
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var recordToEdit = _context.Movies
+            .Single(m => m.MovieId == id);
         
-        return View("Confirmation", response);
+        ViewBag.Categories = _context.Categories.ToList();
+        
+        return View("AddMovieForm", recordToEdit);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Movie updatedMovie)
+    {
+        _context.Update(updatedMovie);
+        _context.SaveChanges();
+        
+        return RedirectToAction("Movielist");
+    }
+    
+    // Delete action for deleting a movie record from the table
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        var recordToDelete = _context.Movies
+            .Single(m => m.MovieId == id);
+        
+        return View(recordToDelete);
+    }
+    
+    // Post for the same action
+    [HttpPost]
+    public IActionResult Delete(Movie deletedMovie)
+    {
+        _context.Movies.Remove(deletedMovie);
+        _context.SaveChanges();
+        
+        return RedirectToAction("Movielist");
     }
 }
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,26 +1,39 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Mission6Assignment.Models;
+
+// HINT, adding a ? at the end of the variable type 
+// for example, int? means that the variable is NOT required
+// for the user to enter in.
 
 public class Movie
 {
     [Key]
     [Required]
     public int MovieId { get; set; }
-    public required string CategoryName { get; set; }
-    public required string MovieTitle { get; set; }
-    public required int? MovieYear { get; set; }
-    public required string DirectorFirstName { get; set; }
-    public required string DirectorLastName { get; set; }
+    
+    [ForeignKey("CategoryId")]
+    public required int? CategoryId { get; set; }
+    public required Category? Category { get; set; }  // Setting up FK relationship to table
+    
+    [Required(ErrorMessage = "Must enter Movie Name to Add Movie.")]
+    public required string Title { get; set; }
 
-    public required string MovieRating { get; set; }
+    [Required(ErrorMessage = "Movie Release Year is required.")]
+    [Range(1888, int.MaxValue, ErrorMessage = "Movie Release Year must be between 1888 and today's date")]
+    public required int Year { get; set; } // Default value is current year
+    public required string? Director { get; set; }
+    public required string? Rating { get; set; }
 
-    public bool Edited { get; set; }
+    [Required(ErrorMessage = "Must note whether the movie has been edited or not.")]
+    public required bool Edited { get; set; }
+    
+    [Required(ErrorMessage = "Must note whether the movie has been copied to plex or not")]
+    public required bool CopiedToPlex { get; set; } // required column for new database
     public string? LentTo { get; set; }
     
     [StringLength(25, ErrorMessage = "Notes must be 25 characters or less.")]
     public string? Notes { get; set; }
 }
-
-// Set up a connection string for SQLite Database in my appsettings.json file
